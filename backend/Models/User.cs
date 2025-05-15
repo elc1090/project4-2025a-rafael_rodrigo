fa0610afb2f31ceee00b168dde984d7bce84bf53
Rafael Carneiro Pregardier <102990182+Pre9o@users.noreply.github.com>
2025-05-15 10:54:40 -0300

    feat: frontend

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,4 +1,15 @@
 ﻿namespace backend.Models {
     public class User {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public User()
+        {
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
     }
 }
